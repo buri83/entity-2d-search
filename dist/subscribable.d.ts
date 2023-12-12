b5b1621632bd6411c8aa14973094fb7cf0e5b535
Your Name <buri.ubuntu@example.com>
2023-12-12 21:28:26 +0900

    add prepare command to package.json

--- dist/subscribable.d.ts
+++
@@ -1,6 +0,0 @@
-export type SubscribableListener<T> = (value: T) => void;
-export declare class Subscribable<T> {
-    private listeners;
-    subscribe(listener: SubscribableListener<T>): void;
-    publish(value: T): void;
-}
